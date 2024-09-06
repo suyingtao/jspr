d4ad13f5c90f555eee6772537fdd36f7e899bed9
suyingtao <suyingtao@sina.cn>
2024-09-06 11:15:27 +0800

    test: add object and array test case

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -58,6 +58,22 @@ describe("expr", () => {
     expect(expr("1 ? 2 ? 3 : 4 : 5 ? 6 : 7")).toBe(1 ? 2 ? 3 : 4 : 5 ? 6 : 7);
     expect(expr("1 ? 2 ? 3 : 4 : 5 ? 6 : 7 ? 8 : 9")).toBe(1 ? 2 ? 3 : 4 : 5 ? 6 : 7 ? 8 : 9);
   });
+
+  test("array", () => {
+    expect(expr("[1, 2, 3]")).toEqual([1, 2, 3]);
+    expect(expr("[1, 2, 3, 4, 5]")).toEqual([1, 2, 3, 4, 5]);
+    expect(expr("[1 + 2]")).toEqual([1 + 2]);
+    expect(expr("[1 + 2, 3 + 4]")).toEqual([1 + 2, 3 + 4]);
+    expect(expr("[1 + 2, 3 + 4, 5 + 6]")).toEqual([1 + 2, 3 + 4, 5 + 6]);
+  });
+
+  test("object", () => {
+    expect(expr('{"a": 1, "b": 2, "c": 3}')).toEqual({ a: 1, b: 2, c: 3 });
+    expect(expr('{"a": 1 + 2, "b": 3 + 4, "c": 5 + 6}')).toEqual({ a: 1 + 2, b: 3 + 4, c: 5 + 6 });
+    expect(expr('{"a": { "b": 1 + 2 }, "c": { "d": 3 + 4 }}')).toEqual({ a: { b: 1 + 2 }, c: { d: 3 + 4 } });
+    expect(expr('{"a": [1, 2, 3], "b": [4, 5, 6]}')).toEqual({ a: [1, 2, 3], b: [4, 5, 6] });
+    expect(expr('{"a": [1 + 2, 3 + 4, 5 + 6], "b": [7 + 8, 9 + 10, 11 + 12]}')).toEqual({ a: [1 + 2, 3 + 4, 5 + 6], b: [7 + 8, 9 + 10, 11 + 12] });
+  });
 });
 
 describe("exprWithContext", () => {
